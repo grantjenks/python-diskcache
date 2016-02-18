18e8c5e74693f16686a5a70688f3724af8ddab56
Grant Jenks <contact@grantjenks.com>
2016-02-17 23:09:06 -0800

    Bump version to 0.4

--- diskcache/__init__.py
+++ diskcache/__init__.py
@@ -4,8 +4,8 @@ from .core import Disk, Cache, EVICTION_POLICY, DEFAULT_SETTINGS, EmptyDirWarnin
 # from .djangocache import DjangoCache
 
 __title__ = 'diskcache'
-__version__ = '0.3.1'
-__build__ = 0x000301
+__version__ = '0.4.0'
+__build__ = 0x000400
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
