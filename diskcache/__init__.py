e61882e87e61049d2df59bf18778a3a724df1910
Grant Jenks <contact@grantjenks.com>
2016-02-22 20:44:05 -0800

    Bump version to 0.7.0

--- diskcache/__init__.py
+++ diskcache/__init__.py
@@ -11,8 +11,8 @@ except ImportError:
 
 
 __title__ = 'diskcache'
-__version__ = '0.6.0'
-__build__ = 0x000600
+__version__ = '0.7.0'
+__build__ = 0x000700
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
