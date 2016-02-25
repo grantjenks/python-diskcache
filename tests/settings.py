634ba72fe4f11a8837a145f0659909061c9a3ee9
Grant Jenks <contact@grantjenks.com>
2016-02-25 10:42:16 -0800

    Move db.sqlite3 under tests directory

--- tests/settings.py
+++ tests/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'tests', 'db.sqlite3'),
     }
 }
 
