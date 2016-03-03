6806d5dbeb09371d0da34e9d3b63ca896b74cf9d
Grant Jenks <contact@grantjenks.com>
2016-03-03 09:46:04 -0800

    Format benchmark glob data for rst

--- tests/benchmark_glob.py
+++ tests/benchmark_glob.py
@@ -7,17 +7,36 @@ import os.path as op
 import shutil
 import timeit
 
+from utils import secs
+
 shutil.rmtree('tmp', ignore_errors=True)
 
 os.mkdir('tmp')
+
+size = 12
+cols = ('Count', 'Time')
+template = ' '.join(['%' + str(size) + 's'] * len(cols))
+
+print()
+print(' '.join(['=' * size] * len(cols)))
+print('Timings for glob.glob1')
+print('-'.join(['-' * size] * len(cols)))
+print(template % ('Count', 'Time'))
+print(' '.join(['=' * size] * len(cols)))
 
 for count in [10 ** exp for exp in range(6)]:
     for value in range(count):
         with open(op.join('tmp', '%s.tmp' % value), 'wb') as writer:
             pass
         
-    delta = timeit.timeit(stmt="glob.glob1('tmp', '*.tmp')", setup='import glob', number=100)
+    delta = timeit.timeit(
+        stmt="glob.glob1('tmp', '*.tmp')",
+        setup='import glob',
+        number=100
+    )
 
-    print('Count %6s Time %.3f' % (count, delta))
+    print(template % (count, secs(delta)))
+
+print(' '.join(['=' * size] * len(cols)))
 
 shutil.rmtree('tmp', ignore_errors=True)
