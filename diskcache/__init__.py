4fc872da99506fc052ffa07137d16a6e7d0ac092
Grant Jenks <contact@grantjenks.com>
2016-07-09 19:50:23 -0700

    Bump version to 1.6.7

--- diskcache/__init__.py
+++ diskcache/__init__.py
@@ -12,8 +12,8 @@ except Exception: # pylint: disable=broad-except
 
 
 __title__ = 'diskcache'
-__version__ = '1.6.6'
-__build__ = 0x010606
+__version__ = '1.6.7'
+__build__ = 0x010607
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
