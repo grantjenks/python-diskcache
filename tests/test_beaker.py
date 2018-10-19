869c7ca79ad838f997ba8607913e94575a159298
Drew J. Sonne <drew.sonne@gmail.com>
2018-10-19 11:37:31 +0100

    Only run beaker tests if beaker is installed

--- tests/test_beaker.py
+++ tests/test_beaker.py
@@ -1,43 +1,48 @@
-from beaker.cache import Cache
+try:
+    from beaker.cache import Cache
+    runBeakerTest = True
+except ImportError:
+    runBeakerTest = False
+
+if runBeakerTest:
+
+    def test_has_key():
+        cache = Cache('test', data_dir='./cache', type='ext:diskcache')
+        o = object()
+        cache.set_value("test", o)
+        assert cache.has_key("test")
+        assert "test" in cache
+        assert not cache.has_key("foo")
+        assert "foo" not in cache
+        cache.remove_value("test")
+        assert not cache.has_key("test")
 
 
-def test_has_key():
-    cache = Cache('test', data_dir='./cache', type='ext:diskcache')
-    o = object()
-    cache.set_value("test", o)
-    assert cache.has_key("test")
-    assert "test" in cache
-    assert not cache.has_key("foo")
-    assert "foo" not in cache
-    cache.remove_value("test")
-    assert not cache.has_key("test")
+    def test_has_key_multicache():
+        cache = Cache('test', data_dir='./cache', type='ext:diskcache')
+        o = object()
+        cache.set_value("test", o)
+        assert cache.has_key("test")
+        assert "test" in cache
+        cache = Cache('test', data_dir='./cache', type='ext:diskcache')
+        assert cache.has_key("test")
+        cache.remove_value('test')
 
 
-def test_has_key_multicache():
-    cache = Cache('test', data_dir='./cache', type='ext:diskcache')
-    o = object()
-    cache.set_value("test", o)
-    assert cache.has_key("test")
-    assert "test" in cache
-    cache = Cache('test', data_dir='./cache', type='ext:diskcache')
-    assert cache.has_key("test")
-    cache.remove_value('test')
+    def test_clear():
+        cache = Cache('test', data_dir='./cache', type='ext:diskcache')
+        o = object()
+        cache.set_value("test", o)
+        assert cache.has_key("test")
+        cache.clear()
+        assert not cache.has_key("test")
 
 
-def test_clear():
-    cache = Cache('test', data_dir='./cache', type='ext:diskcache')
-    o = object()
-    cache.set_value("test", o)
-    assert cache.has_key("test")
-    cache.clear()
-    assert not cache.has_key("test")
-
-
-def test_unicode_keys():
-    cache = Cache('test', data_dir='./cache', type='ext:diskcache')
-    o = object()
-    cache.set_value(u_('hiŏ'), o)
-    assert u_('hiŏ') in cache
-    assert u_('hŏa') not in cache
-    cache.remove_value(u_('hiŏ'))
-    assert u_('hiŏ') not in cache
+    def test_unicode_keys():
+        cache = Cache('test', data_dir='./cache', type='ext:diskcache')
+        o = object()
+        cache.set_value(u_('hiŏ'), o)
+        assert u_('hiŏ') in cache
+        assert u_('hŏa') not in cache
+        cache.remove_value(u_('hiŏ'))
+        assert u_('hiŏ') not in cache
