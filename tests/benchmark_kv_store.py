8d2009abd1e92875a31b21bdf2799ea757fb6b24
Grant Jenks <grant.jenks@gmail.com>
2021-01-22 11:19:45 -0800

    Rewrite k/v store benchmarking to avoid IPython "magic" syntax

--- tests/benchmark_kv_store.py
+++ tests/benchmark_kv_store.py
@@ -1,48 +1,78 @@
-import dbm
+"""Benchmarking Key-Value Stores
+
+$ python -m IPython tests/benchmark_kv_store.py
+
+"""
+
 import diskcache
-import pickledb
-import shelve
-import sqlitedict
-import timeit
+
+from IPython import get_ipython
+
+ipython = get_ipython()
+assert ipython is not None, 'No IPython! Run with $ ipython ...'
 
 value = 'value'
 
 print('diskcache set')
 dc = diskcache.FanoutCache('/tmp/diskcache')
-%timeit -n 100 -r 7 dc['key'] = value
+ipython.magic("timeit -n 100 -r 7 dc['key'] = value")
 print('diskcache get')
-%timeit -n 100 -r 7 dc['key']
+ipython.magic("timeit -n 100 -r 7 dc['key']")
 print('diskcache set/delete')
-%timeit -n 100 -r 7 dc['key'] = value; del dc['key']
+ipython.magic("timeit -n 100 -r 7 dc['key'] = value; del dc['key']")
 
-print('dbm set')
-d = dbm.open('/tmp/dbm', 'c')
-%timeit -n 100 -r 7 d['key'] = value; d.sync()
-print('dbm get')
-%timeit -n 100 -r 7 d['key']
-print('dbm set/delete')
-%timeit -n 100 -r 7 d['key'] = value; del d['key']; d.sync()
+try:
+    import dbm.gnu  # Only trust GNU DBM
+except ImportError:
+    print('Error: Cannot import dbm.gnu')
+    print('Error: Skipping import shelve')
+else:
+    print('dbm set')
+    d = dbm.gnu.open('/tmp/dbm', 'c')
+    ipython.magic("timeit -n 100 -r 7 d['key'] = value; d.sync()")
+    print('dbm get')
+    ipython.magic("timeit -n 100 -r 7 d['key']")
+    print('dbm set/delete')
+    ipython.magic(
+        "timeit -n 100 -r 7 d['key'] = value; d.sync(); del d['key']; d.sync()"
+    )
 
-print('shelve set')
-s = shelve.open('/tmp/shelve')
-%timeit -n 100 -r 7 s['key'] = value; s.sync()
-print('shelve get')
-%timeit -n 100 -r 7 s['key']
-print('shelve set/delete')
-%timeit -n 100 -r 7 s['key'] = value; del s['key']; s.sync()
+    import shelve
 
-print('sqlitedict set')
-sd = sqlitedict.SqliteDict('/tmp/sqlitedict', autocommit=True)
-%timeit -n 100 -r 7 sd['key'] = value
-print('sqlitedict get')
-%timeit -n 100 -r 7 sd['key']
-print('sqlitedict set/delete')
-%timeit -n 100 -r 7 sd['key'] = value; del sd['key']
+    print('shelve set')
+    s = shelve.open('/tmp/shelve')
+    ipython.magic("timeit -n 100 -r 7 s['key'] = value; s.sync()")
+    print('shelve get')
+    ipython.magic("timeit -n 100 -r 7 s['key']")
+    print('shelve set/delete')
+    ipython.magic(
+        "timeit -n 100 -r 7 s['key'] = value; s.sync(); del s['key']; s.sync()"
+    )
 
-print('pickledb set')
-p = pickledb.load('/tmp/pickledb', True)
-%timeit -n 100 -r 7 p['key'] = value
-print('pickledb get')
-%timeit -n 100 -r 7 p = pickledb.load('/tmp/pickledb', True); p['key']
-print('pickledb set/delete')
-%timeit -n 100 -r 7 p['key'] = value; del p['key']
+try:
+    import sqlitedict
+except ImportError:
+    print('Error: Cannot import sqlitedict')
+else:
+    print('sqlitedict set')
+    sd = sqlitedict.SqliteDict('/tmp/sqlitedict', autocommit=True)
+    ipython.magic("timeit -n 100 -r 7 sd['key'] = value")
+    print('sqlitedict get')
+    ipython.magic("timeit -n 100 -r 7 sd['key']")
+    print('sqlitedict set/delete')
+    ipython.magic("timeit -n 100 -r 7 sd['key'] = value; del sd['key']")
+
+try:
+    import pickledb
+except ImportError:
+    print('Error: Cannot import pickledb')
+else:
+    print('pickledb set')
+    p = pickledb.load('/tmp/pickledb', True)
+    ipython.magic("timeit -n 100 -r 7 p['key'] = value")
+    print('pickledb get')
+    ipython.magic(
+        "timeit -n 100 -r 7 p = pickledb.load('/tmp/pickledb', True); p['key']"
+    )
+    print('pickledb set/delete')
+    ipython.magic("timeit -n 100 -r 7 p['key'] = value; del p['key']")
