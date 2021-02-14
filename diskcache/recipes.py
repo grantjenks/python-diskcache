80d7bccaa5c6c00ed57c5499821375aebdd80447
Grant Jenks <grant.jenks@gmail.com>
2021-02-13 22:13:17 -0800

    Use metaclass with wrapped thing

--- diskcache/recipes.py
+++ diskcache/recipes.py
@@ -479,32 +479,44 @@ from diskcache import Cache
 
 class AsyncMeta(type):
     def __new__(cls, name, bases, attrs):
-        base, = bases
+        base = attrs['__wrapped__']
 
         def __init__(self, loop=None, executor=None):
-            """Initialize async variant.
-
-            :param loop: asyncio event loop
-            :param executor: concurrent.futures executor
-
-            """
             if loop is None:
                 loop = asyncio.get_event_loop()
             run = functools.partial(loop.run_in_executor, executor)
-            self._async_meta_run = run
+            self.__dict__['_async_meta_run'] = run
+            self.__dict__['_async_meta_thing'] = None
 
         assert '__init__' not in attrs
         attrs['__init__'] = __init__
 
-        @functools.wraps(base.__init__)
         async def initialize(self, *args, **kwargs):
-            """Async initializer for the base class."""
-            func = functools.partial(base.__init__, self, *args, **kwargs)
-            await self._async_meta_run(func)
+            """Initialize the thing attribute."""
+            func = functools.partial(base, *args, **kwargs)
+            thing = await self._async_meta_run(func)
+            self.__dict__['_async_meta_thing'] = thing
 
-        initialize.__name__ = 'initialize'
         assert 'initialize' not in attrs
         attrs['initialize'] = initialize
+
+        def __getattr__(self, name):
+            return getattr(self._async_meta_thing, name)
+
+        assert '__getattr__' not in attrs
+        attrs['__getattr__'] = __getattr__
+
+        def __setattr__(self, name, value):
+            setattr(self._async_meta_thing, name, value)
+
+        assert '__setattr__' not in attrs
+        attrs['__setattr__'] = __setattr__
+
+        def __delattr__(self, name):
+            delattr(self._async_meta_thing, name)
+
+        assert '__delattr__' not in attrs
+        attrs['__delattr__'] = __delattr__
 
         # TODO: Add support for __aiter__, __anext__, __aenter__, and __aexit__
 
@@ -513,11 +525,13 @@ class AsyncMeta(type):
             @functools.wraps(func)
             async def method(self, *args, **kwargs):
                 """Async wrapper method."""
-                call = functools.partial(func, self, *args, **kwargs)
+                # `AsyncThing` wraps `Thing` so pass `self._async_meta_thing` as the
+                # first argument to be bound to `self` in `Thing` method calls.
+                call = functools.partial(func, self._async_meta_thing, *args, **kwargs)
                 return await self._async_meta_run(call)
             return method
 
-        # Iterate the attributes of the thing and make async methods.
+        # Iterate the attributes of the cache and make methods for `AsyncCache`.
 
         for name in dir(base):
             if name.startswith('_'):
@@ -526,14 +540,16 @@ class AsyncMeta(type):
             attr = getattr(base, name)
             if not isinstance(attr, types.FunctionType):
                 continue
+            # TODO: Add support for contextlib.contextmanager types.
             method = make_method(attr)
             attrs[name] = method
 
+        bases = (object,)
         return super().__new__(cls, name, bases, attrs)
 
 
-class AsyncCache(Cache, metaclass=AsyncMeta):
-    pass
+class AsyncCache(metaclass=AsyncMeta):
+    __wrapped__ = Cache
 
 
 ###############################################################################
@@ -542,8 +558,12 @@ class AsyncCache(Cache, metaclass=AsyncMeta):
 async def main():
     cache = AsyncCache()
     await cache.initialize(directory='/tmp/diskcache/async')
+    assert cache.directory == '/tmp/diskcache/async'
     await cache.set('key', 'value')
     print(await cache.get('key'))
+    async with cache.transact():
+        pass
+
 
 
 if __name__ == '__main__':
