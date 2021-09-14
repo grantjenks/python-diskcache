c22d3ee59ee28bd58aec59182d375b1eccd191f2
Grant Jenks <grant.jenks@gmail.com>
2021-09-13 22:03:57 -0700

    Fixup formatting for project urls

--- setup.py
+++ setup.py
@@ -29,12 +29,13 @@ setup(
     long_description=readme,
     author='Grant Jenks',
     author_email='contact@grantjenks.com',
-    url='http://www.grantjenks.com/docs/diskcache/', 
-    project_urls = {
-        'Documentation':'http://www.grantjenks.com/docs/diskcache/',
-        'Source':'https://github.com/grantjenks/python-diskcache',
-        'Tracker':'https://github.com/grantjenks/python-diskcache/issues',
-        'Funding':'https://gumroad.com/l/diskcache',}
+    url='http://www.grantjenks.com/docs/diskcache/',
+    project_urls={
+        'Documentation': 'http://www.grantjenks.com/docs/diskcache/',
+        'Funding': 'https://gum.co/diskcache',
+        'Source': 'https://github.com/grantjenks/python-diskcache',
+        'Tracker': 'https://github.com/grantjenks/python-diskcache/issues',
+    },
     license='Apache 2.0',
     packages=['diskcache'],
     tests_require=['tox'],
